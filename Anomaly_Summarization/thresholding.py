0156a055e60b8592676b8bfca86b420324ab3697
Sauron-Kelsier <sukalyan.bhakat@gmail.com>
2018-06-29 13:42:03 +0530

    Add files via upload

---
+++ Anomaly_Summarization/thresholding.py
@@ -0,0 +1,238 @@
+# BASED ON THE CLASSIFICATION OF THE THRESHOLDING MECHANISM, CREATES SUMMARY
+# CREATES AN OUTPUT VIDEO BY CONCATENATING THE FRAMES IN THE ORIGINAL ORDER
+
+import numpy as np
+import os
+import statistics
+import sys
+from sklearn.metrics import roc_curve
+from sklearn.metrics import auc
+
+
+dataset = "inhouse"
+factor = 1
+time_length = 9
+video_root_path ='/media/hdd2/sukalyan/result'
+
+# function to print the elements of a list (debugging)
+def print_list(list_var):
+
+	for i in list_var:
+		print(i)
+
+# function that loads the reconstruction error values from a file 
+# it also returns the median and the standard deviation
+def load_from_file():
+	import os
+	import sys
+
+	# path where the output files are
+	path = os.path.join(video_root_path,'{0}/result/{1}.txt'.format(dataset,sys.argv[1])) 
+
+	f = open(path,"r")	
+
+	error_list = ([float((line.split('\n')[0]).split(',')[0]) for line in f])
+	sorted_error_list = sorted(error_list)
+	median_val = statistics.median(sorted_error_list)
+	standard_deviation_val = statistics.pstdev(sorted_error_list)
+	variance_val = statistics.pvariance(sorted_error_list)
+	mean_val = statistics.mean(sorted_error_list)
+
+	return median_val,standard_deviation_val,variance_val,mean_val
+
+
+def create_movie():
+	import cv2
+
+	test_video_path = os.path.join(video_root_path,'{0}/testing_frames/jpg/{1}'.format(dataset,sys.argv[1]))
+
+	count = 0
+	output = sys.argv[1] + '_new.mp4'
+	images = []
+	for f in os.listdir(test_video_path):
+	    images.append(f)
+	    break
+
+	# Determine the width and height from the first image
+	image_path = os.path.join(test_video_path, images[0])
+	frame = cv2.imread(image_path)
+	height, width, channels = frame.shape
+	fourcc = cv2.VideoWriter_fourcc(*'mp4v') # Be sure to use lower case
+	out = cv2.VideoWriter(output, fourcc, 20.0, (width, height))
+
+	# iterating over all the images (selecting only the required ones)
+	for frame_folder in sorted(os.listdir(test_video_path)):
+		image_path = os.path.join(test_video_path, frame_folder)
+		frame = cv2.imread(image_path)
+		out.write(frame) # Write out frame to video
+		count = (count + 1 ) % factor
+
+
+# function that assigns 0/1 to each frame and prints the f-measure by comparing the prediction with the ground truth
+def classification_module(threshold):
+	import sys
+	import os
+	import cv2
+
+	# for prediction and calculating f-measure
+	from sklearn.metrics import f1_score
+	from sklearn.cluster import KMeans
+
+	# for ROC curve
+	from sklearn.metrics import roc_curve
+	import matplotlib.pyplot as plt
+	from sklearn.metrics import auc
+
+	# location of the video files
+	video_root_path ='/media/hdd2/sukalyan/input_videos'
+	result_path = '/media/hdd2/sukalyan/result'
+
+	# location of the groundtruth files
+	ground_truth_path = os.path.join(video_root_path,'{0}/groundtruth/{1}.txt'.format(dataset,sys.argv[1]))
+	result_path = os.path.join(result_path,'{0}/result/{1}.txt'.format(dataset,sys.argv[1]))
+
+	output_val = []
+	output_frame = []
+
+	# loading the output data
+	output_file = open(result_path,"r")
+
+	# LOADING THE RECONSTRUCTION ERROR AND FRAME NUMBER INTO VARIABLES
+	row = 0
+	for line in output_file:
+		x = line.split('\n')		
+		x,y = x[0].split(',')
+	
+		output_val.append(float(x))
+		output_frame.append(int(y))
+
+		row = row + 1
+
+	output_file.close()	
+	
+
+
+######################################################################################################################
+	# setting up for video creation
+
+	test_video_path = os.path.join(video_root_path,'{0}/testing_frames/jpg/{1}'.format(dataset,sys.argv[1]))
+
+	count = 0
+	output = sys.argv[1] + '_new.avi'
+
+	images = []
+	for f in os.listdir(test_video_path):
+	    images.append(f)
+	    break
+
+	# Determine the width and height from the first image
+	image_path = os.path.join(test_video_path, images[0])
+	frame = cv2.imread(image_path)
+	height, width, channels = frame.shape
+	fourcc = cv2.VideoWriter_fourcc(*'mp4v') # Be sure to use lower case
+	out = cv2.VideoWriter(output, fourcc, 20.0, (width, height))
+
+######################################################################################################################	
+
+	# list that will store the final classification
+	result = [0] * row
+	for i in range(0,row):		
+		if(output_val[i] >= threshold):
+
+			index = (i * factor) + 1
+			result[output_frame[i]-1] = 1
+			image_loc = '{0}'.format(output_frame[i]+1+time_length) + '.jpg'
+			image_path = os.path.join(test_video_path, image_loc )
+			frame = cv2.imread(image_path)
+			out.write(frame) # Write out frame to video
+			
+
+	
+	
+# GRAD-CAM
+
+######################################################################################################################
+	# setting up for video creation (GRAD-CAM VIDEOS)
+
+	test_video_path = os.path.join(video_root_path,'{0}/grad_cam/{1}'.format(dataset,sys.argv[1]))
+
+	count = 0
+	output = sys.argv[1] + '_new_grad.avi'
+
+	images = []
+
+	for f in sorted(os.listdir(test_video_path)):	
+		images.append(f)
+	
+	# Determine the width and height from the first image
+	image_path = os.path.join(test_video_path, images[0])
+	frame = cv2.imread(image_path)
+	height, width, channels = frame.shape
+	fourcc = cv2.VideoWriter_fourcc(*'mp4v') # Be sure to use lower case
+	out = cv2.VideoWriter(output, fourcc, 20.0, (width, height))
+
+######################################################################################################################	
+
+	# CONCATENATING TO CREATE A MOVIE
+	
+	for i in range(0,len(images)):
+		image_loc = '{0}'.format(i+1) + '.png'
+		image_path = os.path.join(test_video_path, image_loc )
+		frame = cv2.imread(image_path)
+		out.write(frame) # Write out frame to video	
+
+	# EVALUATION OF THE MODEL USING F-MEASURE
+	ground_truth_file = open(ground_truth_path,"r")
+	
+	# loading the ground truth values into a list
+	ground_truth_val = list((int(line.split('\n')[0]) for line in ground_truth_file))
+	ground_truth_file.close()
+
+	true_positive = 0
+	false_positive = 0
+	true_negative = 0
+	false_negative = 0
+
+	# calculating true positive, true negative, false positives and false negatives
+	for i in range(0,row):
+   		if(ground_truth_val[i]==1):
+   			if(result[i]==ground_truth_val[i]):
+   				true_positive = true_positive + 1
+   			else:
+   				false_positive = false_positive + 1
+   		else:
+   			if(result[i]==ground_truth_val[i]):
+   				true_negative = true_negative + 1
+   			else:
+   				false_negative = false_negative + 1
+
+	ground_truth_val = ground_truth_val[time_length : row + time_length]
+	
+	f_measure = f1_score(ground_truth_val, result, average='weighted')
+
+	fpr, tpr, thresholds = roc_curve(ground_truth_val, result)
+	roc_auc = auc(fpr, tpr,reorder=True)
+	
+	print("\n\n")
+	print("True Positive: " + str(true_positive))
+	print("False Positive: " + str(false_positive))
+	print("True Negative: " + str(true_negative))
+	print("False Negative: " + str(false_negative))
+	print("F-Measure: " + str(f_measure))
+	print("AUC of ROC: " + str(roc_auc))
+	print("\n\n")
+
+
+	
+# MAIN PART OF THE PROGRAM (DRIVER)
+median_value,standard_deviation_value,variance_value,mean_value = load_from_file()
+
+print("Median value is: " + str(median_value))
+print("Standard Deviation value is: " + str(standard_deviation_value))
+print("Variance value is: "  + str(variance_value))
+print("Mean value is: " + str(mean_value))
+
+
+threshold = median_value +  (0.5 * standard_deviation_value)
+print(sys.argv[1] + ".txt     Threshold: " + str(threshold) + "\n\n")
+classification_module(threshold)
